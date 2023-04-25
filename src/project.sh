85fda6a054a196b6e5f559e79c649967e4b431cd
Pekka Röyttä <roytta@gmail.com>
2023-04-25 11:08:34 +0300

    create echo server. test echo server.

--- src/project.sh
+++ src/project.sh
@@ -7,5 +7,29 @@ if [[ ! -e $FIRST_RUN_FILE ]]; then
   touch $FIRST_RUN_FILE
 fi
 
-echo "NOT IMPLEMENTED!" >&2
-exit 1
+_stop_echo_server() {
+     if [ -f /tmp/project-echo-server.pid ]; then
+        echo "Stopping echo server"
+        kill $(cat /tmp/project-echo-server.pid)
+        rm /tmp/project-echo-server.pid
+    else
+        echo "Echo server is not running"
+    fi
+}
+
+case $1 in
+    start-echo-server)
+        echo "Starting echo server"
+        PORT=2000
+        nohup nc localhost -lk $PORT -c 'xargs -n1 -r echo' >/dev/null 2>&1 &
+        echo $! > /tmp/project-echo-server.pid || true
+        echo "$PORT" >&2
+    ;;
+    stop-echo-server)
+        _stop_echo_server
+    ;;
+    *)
+        echo "NOT IMPLEMENTED!" >&2
+        exit 1
+    ;;
+esac
